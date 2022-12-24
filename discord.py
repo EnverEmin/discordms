4614756db3adea49d2f2a6dbe6ce3705a0e68b53
EnverEmin <enveremin1149@gmail.com>
2022-12-24 03:18:56 +0300

    Create discord.py

---
+++ discord.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+# Discord botunuzun tokenini ve prefixini buraya girin
+TOKEN = 'NTU3NjAwMzkzODI0MDQzMDE5.GOU8iv.kNONyG65VjX67yB7dhn-hB6kTdis_OwNop1zNM'
+PREFIX = '!'
+
+bot = commands.Bot(command_prefix=PREFIX)
+
+@bot.command()
+async def merhaba(ctx):
+  await ctx.send('Merhaba!')
+
+bot.run(TOKEN)
