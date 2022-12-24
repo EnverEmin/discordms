426c7988ec95d9453e8f6e049003126bac741eaf
EnverEmin <enveremin1149@gmail.com>
2022-12-24 03:55:56 +0300

    Update discord.py

--- discord.py
+++ discord.py
@@ -1,8 +1,8 @@
-import discord.py
+import discord
 from discord.ext import commands
 
 # Discord botunuzun tokenini ve prefixini buraya girin
-TOKEN = 'NTU3NjAwMzkzODI0MDQzMDE5.GOU8iv.kNONyG65VjX67yB7dhn-hB6kTdis_OwNop1zNM'
+TOKEN = 'YOUR_BOT_TOKEN'
 PREFIX = '!'
 
 bot = commands.Bot(command_prefix=PREFIX)
